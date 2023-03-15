f50d3310c10e223f43d4845a32fa648ce674d0f2
pravin9360 <102347946+pravin9360@users.noreply.github.com>
2023-03-15 14:40:24 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container padding">
+      <div className="col-md-12">
+        <div className="row mx-0">
+         
+         <h1> NEWS LETTER</h1>
+         {/* <br></br> */}
+        </div>
+      </div>
+      <div classname="about">
+         <p>A news letter is a publication with a paper cover which is issused regularly twice a
+year and which contains unique idea of people. It may be a articles, stories, photography,
+advertisements etc., Usually, a article is between 1,000 and 5,000 words, either short or
+long, depending on the subject. Newspaper reporters are trained to give the most important
+points first in a news story. However, articles often begin with a tantalizing mystery,
+question or scenario to hook the reader into reading the entire article. Moreover, newspaper
+reports are objective records of events while magazine articles, often subjective, deal with
+a specific subject, and take a specific angle or point of view.</p>
+            
+
+</div>
+    </div>
+    
+  );
+};
+
+export default About;
